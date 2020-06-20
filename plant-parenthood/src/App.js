2411c2b282907c9acaf13fbcd3fef36ef3b17b37
Ryan McInnis <ryan.mcinnis42@gmail.com>
2020-06-19 20:23:45 -0700

    reset app component

--- plant-parenthood/src/App.js
+++ plant-parenthood/src/App.js
@@ -1,9 +1,9 @@
 import React from "react";
-import Login from './components/Login'
+
 import "./App.css";
 
 function App() {
-  return <div className="App"><Login/></div>;
+  return <div className="App"></div>;
 }
 
 export default App;
