132e0487eb31a43c88242edd50677bfe6516652d
Ryan McInnis <ryan.mcinnis42@gmail.com>
2020-06-22 20:34:00 -0700

    styling for login and signup forms done

--- plant-parenthood/src/App.js
+++ plant-parenthood/src/App.js
@@ -1,9 +1,9 @@
 import React from "react";
-
+import Login from './components/Login'
 import "./App.css";
 
 function App() {
-  return <div className="App"></div>;
+  return <div className="App"><Login /></div>;
 }
 
 export default App;
--- plant-parenthood/src/components/Login.js
+++ plant-parenthood/src/components/Login.js
@@ -1,9 +1,10 @@
 import React, {useState, useEffect} from 'react';
 import * as yup from 'yup';
+import {Container, Form, Label, Input, Title, LightTitle, Button, Active, Error} from '../styles/forms'
 
 const formSchema = yup.object().shape({
-  username: yup.string().required(),
-  password: yup.string().required()
+  username: yup.string().required('Must add a username'),
+  password: yup.string().required('Must include password')
 })
 
 const Login = props => {
@@ -14,13 +15,38 @@ const Login = props => {
 
   const [buttonDisabled, setButtonDisabled] = useState(true);
 
+  const [errors, setErrors] = useState({
+    username: '',
+    password: ''
+  })
+
   useEffect(() => {
     formSchema.isValid(login).then(valid => {
       setButtonDisabled(!valid)
     })
   }, [login])
 
+  const validateChange = e => {
+    yup
+      .reach(formSchema, e.target.name)
+      .validate(e.target.value)
+      .then(valid => {
+        setErrors({
+          ...errors, 
+          [e.target.name]: ''
+        })
+      })
+      .catch(err => {
+        setErrors({
+          ...errors,
+          [e.target.name]: err.errors[0]
+        })
+      })
+  }
+
   const handleChange = e => {
+    e.persist();
+    validateChange(e);
     setLogin({...login, [e.target.name]: e.target.value})
     console.log(login)
   }
@@ -35,21 +61,21 @@ const Login = props => {
   }
 
   return (
-    <div>
-      <h3>Welcome back!</h3>
-      <h3>Log into your account</h3>
-      <form onSubmit={handleSubmit}>
-        <label htmlFor='username'>
-          Username
-          <input id='username' type='text' name='username' value={login.username} onChange={handleChange} />
-        </label>
-        <label htmlFor='password'>
-          Password
-          <input id='password' type='password' name='password' value={login.password} onChange={handleChange} />
-        </label>
-        <button type='submit' disabled={buttonDisabled}>Next</button>
-      </form>
-    </div>
+    <Container>
+      <Title>Welcome back!</Title>
+      <LightTitle>Log into your account</LightTitle>
+      <Form onSubmit={handleSubmit}>
+        <Label htmlFor='username'>Username</Label>
+        <Input id='username' type='text' name='username' value={login.username} onChange={handleChange} />
+        {errors.username.length > 0 ? (<Error>{errors.username}</Error>) : null}
+
+        <Label htmlFor='password'>Password</Label>
+        <Input id='password' type='password' name='password' value={login.password} onChange={handleChange} />
+        {errors.password.length > 0 ? (<Error>{errors.password}</Error>) : null}
+        
+        {buttonDisabled === true ? (<Button type='submit' disabled={buttonDisabled}>Next</Button>) : (<Active type='submit' disabled={buttonDisabled}>Next</Active>) }
+      </Form>
+    </Container>
   )
 }
 
